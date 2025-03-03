f32cbd716ae20b54cb9126e1a668f7743639b11c
MiguelAriza77 <112282496+MiguelAriza77@users.noreply.github.com>
2025-03-03 15:21:13 -0500

    AGREGADO: mecánica caminar y correr

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputs inputs;
+    private Vector2 moveInput;
+    public float speed = 2f;
+
+    private void Awake()
     {
-        
+        inputs = new PlayerInputs();
+        inputs.Movement.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (inputs.Movement.Run.IsPressed())
+        {
+            speed = 4f;
+        }
+        else
+        {
+            speed = 2f;
+        }
+
+        moveInput = inputs.Movement.Walk.ReadValue<Vector2>().normalized;
+        Vector3 moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        if (moveDirection.sqrMagnitude > 0.01f) // Evita rotar si no hay movimiento
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+        }
+
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
     }
 }
